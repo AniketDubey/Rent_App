51558185476a17618081c39782c213b92b816202
AniketDubey <aniket.dubey0206@gmail.com>
2021-08-25 13:44:38 +0530

    Added State Management

---
+++ lib/screens/Add_Transaction_Screen.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class Add_Transaction extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          "Add Transaction",
+        ),
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(10),
+        child: Form(child: ),
+      ),
+    );
+  }
+}
--- lib/screens/PersonalDetailsScreen.dart
+++ lib/screens/PersonalDetailsScreen.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:intl/intl.dart';
 
 import 'package:rentapp/models/data/DUMMYDATA.dart';
 import 'package:rentapp/models/skeleton/basic_summary.dart';
 import 'package:rentapp/models/skeleton/transaction_summary.dart';
+
+import 'package:provider/provider.dart';
 
 class PersonalDetailsScreen extends StatelessWidget {
   final String id;
@@ -11,13 +12,32 @@ class PersonalDetailsScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final BasicSummary _customer =
-        DUMMYDATA.firstWhere((element) => element.id == id);
+    /* final BasicSummary _customer =
+        DUMMYDATA.firstWhere((element) => element.id == id); 
 
+
+    final List<Transaction> _history = _customer.trandetails;*/
+
+    final BasicSummary _customer = Provider.of<BSummary>(
+      context,
+      listen: false,
+    ).findByID(id);
     final List<Transaction> _history = _customer.trandetails;
+    _history.sort((a, b) {
+      return -a.date.compareTo(b.date);
+    });
 
     return Scaffold(
       appBar: AppBar(
+        actions: <Widget>[
+          Padding(
+            padding: EdgeInsets.all(10),
+            child: IconButton(
+              onPressed: () {},
+              icon: Icon(Icons.add),
+            ),
+          ),
+        ],
         title: Text("${_customer.name}"),
       ),
       body: Padding(
